686601cb6e952851c95ccde1e02d18d965c741bd
Xiaming Chen <chenxm35@gmail.com>
2015-02-05 00:59:55 +0800

    Add documentation and basic package structure.

---
+++ R/movr.r
@@ -0,0 +1,9 @@
+#' movr: inspecting human mobility with R
+#'
+#' A package targeting at analyzing, modeling, and visualizing
+#' human mobility from temporal and spatial perspectives.
+#'
+#' @name movr
+#' @docType package
+#' @import dplyr
+NULL
